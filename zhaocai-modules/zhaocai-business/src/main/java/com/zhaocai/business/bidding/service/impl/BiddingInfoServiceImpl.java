9d23581320b03f49ab1ab6d27280389a45023651
zs <1371576853@qq.com>
2024-10-06 20:17:41 +0800

    bug修复

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/bidding/service/impl/BiddingInfoServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/bidding/service/impl/BiddingInfoServiceImpl.java
@@ -144,7 +144,8 @@ public class BiddingInfoServiceImpl extends ServiceImpl<BiddingInfoMapper,Biddin
                 .eq(BiddingEvaluatExpert::getNoticeId, queryVO.getNoticeId())
                 .eq(BiddingEvaluatExpert::getIsJoin, 1));
         //查询首轮供应商报价数据，有首轮数据才能在列表中显示（且状态不为废标）
-        List<BiddingQuotationListVO> list = baseMapper.findBiddingQuotationList(queryVO);
+//        List<BiddingQuotationListVO> list = baseMapper.findBiddingQuotationList(queryVO);
+        List<BiddingQuotationListVO> list = baseMapper.findBiddingQuotationListTongJi(queryVO);
 
 
         /* 按供应商分组 将招标对象 的 投标数据 分组，并按版本号排序。 */
