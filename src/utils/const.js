605fff248783fca8139132a63164fbe8edbaecda
zs <1371576853@qq.com>
2024-10-21 17:55:59 +0800

    dev_future分支测试环境联想文档配置

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 //文件上传地址
-export const uploadFileUrl = "/prod-api/file/upload";
+export const uploadFileUrl = "/dev-api/file/upload";
 //文件服务公用地址
 export const offerService = "http://192.168.241.29:8001/docs/app";
 //文件服务应用名
