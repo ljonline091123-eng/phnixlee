8cc01c4fea2d53c1ef6cba07ec060247bc2ae178
zs <1371576853@qq.com>
2024-10-21 17:51:51 +0800

    删除打包文件

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 //文件上传地址
-export const uploadFileUrl = process.env.NODE_ENV === "staging"?"/prod-api/file/upload":"/dev-api/file/upload";
+export const uploadFileUrl = "/prod-api/file/upload";
 //文件服务公用地址
-export const offerService = process.env.NODE_ENV === "staging"?"http://192.168.241.29:8001/docs/app":"https://ck.hncig.cn:60023/docs/app";
+export const offerService = "http://192.168.241.29:8001/docs/app";
 //文件服务应用名
-export const offerRepo = process.env.NODE_ENV === "staging"?"thirdparty-rest1":"zhaocai-prod";
+export const offerRepo = "thirdparty-rest1";
