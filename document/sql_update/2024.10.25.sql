adc5a5dcd99b089ea7ec285358b8a0f6fe61b7d5
zs <1371576853@qq.com>
2024-10-25 16:22:39 +0800

    新增专家增加流程sql脚本xml更新

---
+++ document/sql_update/2024.10.25.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `hnjiantou-zhaocai-dev`.tb_expert ADD state int NULL COMMENT '审批状态';
+ALTER TABLE `hnjiantou-zhaocai-dev`.tb_expert ADD wf_process_id varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '流程实例 id';
+ALTER TABLE `hnjiantou-zhaocai-dev`.tb_expert ADD process_type int NULL COMMENT '流程类型';
+ALTER TABLE `hnjiantou-zhaocai-dev`.tb_expert ADD operate_comment varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '审批批语';
