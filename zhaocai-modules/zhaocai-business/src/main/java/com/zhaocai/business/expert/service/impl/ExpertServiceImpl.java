58476cd7a0691cf428f93c52adc5272009244774
zs <1371576853@qq.com>
2024-10-29 14:21:41 +0800

    专家提交info

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/expert/service/impl/ExpertServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/expert/service/impl/ExpertServiceImpl.java
@@ -269,6 +269,8 @@ public class ExpertServiceImpl extends ServiceImpl<ExpertMapper,Expert> implemen
         attachmentService.addAttachment(expertVO.getResumeAttachList(), AttachmentTypeEnum.EXPERT_RESUME, expert.getId());
 
         System.out.println("[专家新增提交]"+(res && (expert!=null && expert.getState()!=null && (submitFlag || expert.getState().equals(ExpertStateEnum.REJECT.getState()) || expert.getState().equals(ExpertStateEnum.SAVE.getState()))) ));
+        System.out.println(res);
+        System.out.println(expert);
 
         if (res && (expert!=null && expert.getState()!=null && (submitFlag || expert.getState().equals(ExpertStateEnum.REJECT.getState()) || expert.getState().equals(ExpertStateEnum.SAVE.getState()))) ){
             //提交审批信息
