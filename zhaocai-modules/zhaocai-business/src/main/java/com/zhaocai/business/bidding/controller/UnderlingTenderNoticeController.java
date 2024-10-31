5f7c328c78d9c4bc4a8e0e55feed48837a835c22
zs <1371576853@qq.com>
2024-10-31 16:03:06 +0800

    获取招标公告列表参数打印

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/bidding/controller/UnderlingTenderNoticeController.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/bidding/controller/UnderlingTenderNoticeController.java
@@ -8,9 +8,15 @@ import com.zhaocai.common.core.bean.PageResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Api(value = "第三方-招标公告接口")
 @RestController
@@ -27,7 +33,23 @@ public class UnderlingTenderNoticeController extends BladeController {
      */
     @GetMapping("/listPage")
     @ApiOperation(value = "获取招标公告列表")
-    public PageResult<TenderNoticeVO> listPage(UnderlingTenderNoticeQueryVO queryVO) {
+    public PageResult<TenderNoticeVO> listPage(UnderlingTenderNoticeQueryVO queryVO, HttpServletRequest request) {
+        System.out.println("[获取招标公告列表]参数打印开始");
+        request.getParameterMap().forEach((key, value) -> {
+            System.out.println(key + " : " + Arrays.toString(value));
+        });
+        System.out.println("[获取招标公告列表]参数打印结束");
         return noticeService.listTenderNoticePage(queryVO);
+    }
+
+    // 处理参数类型转换异常
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleTypeMismatchException(MethodArgumentTypeMismatchException ex, HttpServletRequest request) {
+        System.out.println("[获取招标公告列表-类型转换异常] 参数信息打印开始");
+        request.getParameterMap().forEach((key, value) -> {
+            System.out.println(key + " : " + Arrays.toString(value));
+        });
+        System.out.println("[获取招标公告列表-类型转换异常] 参数信息打印结束");
+        return ResponseEntity.badRequest().body("参数类型错误: " + ex.getMessage());
     }
 }
