e5f2e39e8c9f64a69662382767e5d06baa3e5f20
zs <1371576853@qq.com>
2024-11-01 17:47:06 +0800

    父项目编码(项目部)使用二级单位

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorChangeBlackServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorChangeBlackServiceImpl.java
@@ -127,7 +127,7 @@ public class VendorChangeBlackServiceImpl extends ServiceImpl<VendorChangeMapper
         paramMap.put("groupId", UserConstants.GROUP_DEPT_ID);/* 集团 */
         paramMap.put("companyId", org);/* 公司 二级单位 */
         paramMap.put("responsibilityDeptId", org);/* 责任单位 三级单位 */
-        paramMap.put("parentProjectCode", null);/* 父项目编码(项目部) */
+        paramMap.put("parentProjectCode", org);/* 父项目编码(项目部) */
 
         processService.startProcessInstance(
                 ProcessKeyEnum.ZHAOCAI_VENDOR_MOVE_INOROUT_BLACK.getIdentifying(),paramMap);
--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorChangeServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorChangeServiceImpl.java
@@ -286,7 +286,7 @@ public class VendorChangeServiceImpl extends ServiceImpl<VendorChangeMapper,Vend
             paramMap.put("groupId", UserConstants.GROUP_DEPT_ID);/* 集团 */
             paramMap.put("companyId", org);/* 公司 二级单位 */
             paramMap.put("responsibilityDeptId", org);/* 责任单位 三级单位 */
-            paramMap.put("parentProjectCode", null);/* 父项目编码(项目部) */
+            paramMap.put("parentProjectCode", org);/* 父项目编码(项目部) */
 
             processService.startProcessInstance(
                     ProcessKeyEnum.ZHAOCAI_VENDOR_UPDATEINFO.getIdentifying(),paramMap);
