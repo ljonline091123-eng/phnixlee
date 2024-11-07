3fef1409627abd6ea7aa6492f0a68479b680868e
liushuangnian <2635340225@qq.com>
2024-11-07 11:30:40 +0800

    易料合同核减校验

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/procurement/service/impl/ContractPlanningSplitServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/procurement/service/impl/ContractPlanningSplitServiceImpl.java
@@ -171,7 +171,7 @@ public class ContractPlanningSplitServiceImpl extends ServiceImpl<ContractPlanni
                 useUp.put(materialsList.getContractSplitId(),false);
             }
         }
-        log.info("[易料合同合约拆分（物料是否已用完）]",useUp.toString());
+        log.info("[易料合同合约拆分（物料是否已用完）]",useUp.keySet());
         // 设置使用金额
         BigDecimal totalUsedAmount = BigDecimal.ZERO;
         Map<Long, BigDecimal> totalAmount = new HashMap<>();
@@ -184,7 +184,7 @@ public class ContractPlanningSplitServiceImpl extends ServiceImpl<ContractPlanni
             }
             totalAmount.put(agreementMaterials.getContractSplitId(), totalUsedAmount);
         }
-        log.info("[易料合同合约拆分（设置使用金额）]",totalAmount.toString());
+        log.info("[易料合同合约拆分（设置使用金额）]",totalAmount.keySet());
         /* 将原来的减去 */
         if (agreementMaterialsListsOld != null) {
             for (AgreementMaterialsList agreementMaterials : agreementMaterialsListsOld) {
@@ -197,7 +197,7 @@ public class ContractPlanningSplitServiceImpl extends ServiceImpl<ContractPlanni
                 totalAmount.put(agreementMaterials.getContractSplitId(), totalUsedAmount);
             }
         }
-        log.info("[易料合同合约拆分（将原来的减去）]", totalAmount.toString());
+        log.info("[易料合同合约拆分（将原来的减去）]", totalAmount.keySet());
 
         totalAmount.forEach((contractSplitId, amount)->{
             int isUseUpState = !useUp.get(contractSplitId) ? 1 : 2;
