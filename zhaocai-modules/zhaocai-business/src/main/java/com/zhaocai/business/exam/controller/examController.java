fffb67ff237e3e664bc906ea51e085b87f4bff2f
xiaoyu <6227797@qq.com>
2024-11-21 14:49:37 +0800

    新建准考证包路径

---
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/exam/controller/examController.java
@@ -0,0 +1,19 @@
+package com.zhaocai.business.exam.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exam/exam")
+public class examController {
+
+    //上传头像
+
+
+    //根据输入的身份证号码查询记录是否存在
+
+
+    //根据输入的身份证号码替换word模板，并生成pdf，然后在浏览器打开
+
+
+}
