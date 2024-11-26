6c502f783d94f56934481234783d059c788c7ef1
陈胜 <345@qq.com>
2024-11-26 10:43:47 +0800

    Dev feature exam

---
+++ src/views/seating/index.vue
@@ -0,0 +1,215 @@
+<template>
+<div  class="app-container">
+         <div  style=" position: absolute;right: 20px;top: 10px;">
+            <el-select style="right: 20px;" v-model="examinationroom" placeholder="请选择">
+                <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+            <el-button
+            style="right: 20px;"
+            type="primary"
+            icon="el-icon-search"
+            size="small"
+            @click="seatingQuery"
+            >搜索</el-button
+            >
+             <el-button  type="primary" size="small" @click="print">打印</el-button>
+           </div>
+    
+  <div id="printMe">
+        <div class="competitionTit" style="padding: 52px 0 22px 8px;">
+            <div ><span style="visibility: hidden;">真真真真真真真真真真真真真真真真</span>湖南建设投资集团第二届商务成控综合技能竞赛{{examinationroomTit}}参赛人员座次表</div>
+        </div>
+        <div style="display: flex;flex-wrap: wrap;width: 100%;padding: 8px 0 12px 0; " >
+            <div style="width: 8.5%;padding: 2px; border: 1px solid #000;text-align: center;color:#666666;font-size: 14px;" v-for="(item,index1) in examinationRoomList" :key="index1">
+                <div style="padding: 12px 0;">座位号：{{item.seatNumber}}号</div>
+                 <img style="padding: 6px 0;" :src="item.pictureUrl" width="90px" height="130px" class="avatar">
+                 <div style="padding: 6px 0;">姓名：{{item.examineeName}}</div>
+                 <div style="padding: 6px 0px;">签字：<span style="visibility: hidden;">姚真真</span></div>
+                 <div style="padding: 6px 0;">{{item.phone}}</div>  
+            </div>
+
+        </div>
+         
+  </div>
+</div>
+</template>
+
+<script>
+
+import { submitExpert,getInfo,saveExpert,checkIdentityCardId,saveExam,repaceWord,getlistByRoom } from "@/api/expert/expert";
+import axios from 'axios'
+export default {
+  name: "competition-certificate",
+
+  data() {
+    
+    return {
+      formData: {
+        identityCardId: "",
+        pictureUrl: "",
+      }, //form表单数据
+      dialogVisible:false,
+      iframeUrls:'',
+      name:'湖南建筑高级技工学校崇技楼六楼',
+      place:'长沙市天心区古堆山巷11号北门',
+      examTime:'2024-11-29 14:30-- 17:30',
+      examinationroom:'第1考场(601)',
+      examinationroomTit:'第1考场(601)',
+      examinationRoomList: [],
+        options: [{
+          value: '第1考场(601)',
+          label: '第1考场(601)'
+        }, {
+          value: '第2考场(602)',
+          label: '第2考场(602)'
+        }, {
+          value: '第3考场(603)',
+          label: '第3考场(603)'
+        }, {
+          value: '第4考场(604)',
+          label: '第4考场(604)'
+        }, {
+          value: '第5考场(605)',
+          label: '第5考场(605)'
+        }, {
+          value: '第6考场(606)',
+          label: '第6考场(606)'
+        }, {
+          value: '第7考场(607)',
+          label: '第7考场(607)'
+        }],
+      rules: {
+        pictureUrl: [
+          {
+            required: true,
+            message: "请上传本人寸照",
+          },
+        ],
+        identityCardId: [
+          {
+            required: true,
+            message: "身份证不能为空",
+          },
+        ],
+       
+      },
+      isSubmit: false,
+    };
+  },
+  created() {
+       this.getlistByRoomFn(this.examinationroom)
+
+  },
+  methods: {
+      print() {
+      let printWindow = window.open('', '_blank');
+      const printContent = document.getElementById('printMe').innerHTML;
+      printWindow.document.write(printContent);
+      printWindow.document.close();
+      printWindow.focus();
+      printWindow.print();
+      printWindow.close();
+    },
+   
+    
+   seatingQuery(){
+        this.getlistByRoomFn(this.examinationroom);
+   },
+    
+    //查看身份证是否存在
+    async getlistByRoomFn(id) {
+        const res = await getlistByRoom(id);
+        this.examinationRoomList=res.data
+        this.examinationroomTit=this.examinationroom
+      },
+
+
+  
+ 
+  },
+  components: {
+
+  },
+};
+</script>
+<style lang="scss" scoped>
+.page-title {
+  width: 100%;
+  border-bottom: solid 1px #ccc;
+  padding: 10px;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &::before {
+    content: "";
+    height: 20px;
+    width: 5px;
+    background-color: rgba(41, 65, 137, 1);
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+
+.form-body {
+  padding: 20px;
+}
+.competitionTit {
+    // max-width: fit-content;
+    // margin-left: auto;
+    // margin-right: auto;
+    color: #2b4acb;
+    font-size: 22px;
+    font-weight: 600;
+    margin-top: 12px;
+
+}
+.competitionName {
+    max-width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+    color: #666666;
+    font-size: 18px;
+    font-weight: 550;
+
+}
+.app-container {
+  overflow: auto; /* 需要滚动时保持这个属性 */
+  scrollbar-width: none; /* 对于Firefox */
+}
+.scroll-container::-webkit-scrollbar {
+  display: none; /* 对于Chrome, Safari和Opera */
+ 
+}
+.dialogtext {
+  width: 100% !important;
+  height: 100% !important;
+}
+  iframe {
+    border: none;
+    width: 100%;
+  }
+  @media print {
+    
+  #printMe {
+    page-break-after: always;
+    //  size: portrait;//纵向
+      size: landscape;//横向
+      text-align: center;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 12px; /* 根据需要调整字体大小 */
+  }
+  /* 其他打印样式 */
+}
+</style>
