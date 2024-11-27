37421916eea0b1631f288fc7d8a5e39222bea8f2
19974999539 <1540125496@qq.com>
2024-11-27 15:35:18 +0800

    更改模板

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/demo/app/DpDemo.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/demo/app/DpDemo.java
@@ -252,7 +252,7 @@ public class DpDemo {
 		// 组织请求参数
 		EditParams params = new EditParams();
 		params.setFilePath(DemoTestFile.getFilePath("服务合同.docx"));
-		params.setFileName("抢抓机遇期培育新动能.docx");
+		params.setFileName("测试.docx");
 		params.setUserInfo("userid1", "用户1");
 		params.setFallbackUrl("http://www.yozosoft.com");
 		params.setUserRight(EditParams.USERRIGHT_EDIT);
@@ -282,7 +282,7 @@ public class DpDemo {
 		// 设置书签时可选择的内容，暂未实现
 	//	params.setBookMarkListRange("甲方,乙方,金额,签订日期");
 		// 设置水印
-		WaterMark wm = new WaterMark(WaterMark.TYPE_TXT, "DEMO-水印");
+		WaterMark wm = new WaterMark(WaterMark.TYPE_TXT, "解文静-水印");
 		// 将水印设置到参数中
 		params.setWaterMark(EditParams.WATER_MARK_TYPE_PAGE, wm);
 
