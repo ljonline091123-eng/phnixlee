5212ee0470cec14fd611d438ac07934ab46463d1
zs <1371576853@qq.com>
2024-11-29 16:31:11 +0800

    控制台打印信息输出2

--- src/views/procurement/bindding.vue
+++ src/views/procurement/bindding.vue
@@ -353,43 +353,6 @@ export default {
       noticeId:''
     };
   },
-  created() {
-    // this.queryParams = {...this.$route.query}
-    // this.getBiddingSchemeList();
-    this.queryParams.procurementType = this.$route.query.procurementType
-    this.queryParams.projectCode = this.$route.query.projectCode
-    this.queryParams.projectCodeList = this.$route.query.projectCodeList
-    this.queryParams.type = this.$route.query.type
-    this.queryParams.noticeStatus = this.$route.query.noticeStatus
-
-    this.getDicts("plan_type").then((res) => {
-      this.bindding_type = res.data;
-    });
-
-    if(this.$route.query.report){
-      this.report=this.$route.query.report
-    }
-      console.log(" this.queryParams.procurementType"+ this.queryParams.procurementType);
-      console.log(" this.queryParams.projectCode"+ this.queryParams.projectCode);
-      console.log(" this.queryParams.projectCodeList"+ this.queryParams.projectCodeList);
-      console.log(" this.queryParams.type"+ this.queryParams.type);
-      console.log(" this.queryParams.noticeStatus"+ this.queryParams.noticeStatus);
-      console.log(" this.queryParams.report"+ this.queryParams.report);
-
-    // if(this.$route.query.report==undefined){
-    //   // const url = 'http://192.168.240.17:31800/ckControl/zbcg/procurement/procurement$bindding?projectCodeList=SG20012024000002-2&type=buildingRate&noticeStatus=8&procurementType=all&report=report&wjSs=%2Fzhaocai%2Fprocurement%2Fbindding';
-    //   const url =window.parent.location.href
-    //   const queryParams = this.parseQuery(url);
-    //   this.queryParams.procurementType = queryParams.procurementType
-    //   this.queryParams.projectCode = queryParams.projectCode
-    //   this.queryParams.projectCodeList = queryParams.projectCodeList
-    //   this.queryParams.type = queryParams.type
-    //   this.queryParams.noticeStatus = queryParams.noticeStatus
-    //   this.report=queryParams.report
-    //   console.log("报表参数",JSON.stringify(queryParams))
-
-    // }
-  },
   computed: {
     ...mapGetters(["project"]),
   },
@@ -580,7 +543,7 @@ export default {
             projectCode: newVal.code,
           };
           console.log('%c👽 handler-》 JSON.stringify(this.queryParams) ', `font-size: 20px;background-color: #f00;`, JSON.stringify(this.queryParams));
-            this.getBiddingSchemeList();
+          this.getBiddingSchemeList();
 
         }
       },
