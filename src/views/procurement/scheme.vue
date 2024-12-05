59fd33890309c1a01c5a7c078065a3fd51107d69
xwj <xwj@qq.com>
2024-12-05 14:34:13 +0800

    文档中台-更新新增合同附件、修改采购方案部分

--- src/views/procurement/scheme.vue
+++ src/views/procurement/scheme.vue
@@ -549,7 +549,7 @@ export default {
       let param = Base64.encode(
         JSON.stringify({ id, procurementType, type: "update" })
       );
-      debugger;
+      // debugger;
       param = encodeURIComponent(param); //避免base64编码中出现"/"时路由404
       this.$router.push(`/procurement/add-scheme/${param}`);
     },
