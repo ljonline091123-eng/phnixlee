b4bd4828ef94afcdb974172bb1062096789c335d
liushuangnian <2635340225@qq.com>
2024-12-09 10:42:31 +0800

    bug修复(供应商注册问题(采购人和供应商都是一个人))

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorContactServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorContactServiceImpl.java
@@ -50,6 +50,7 @@ import com.zhaocai.common.signature.dto.command.PersonAuthCommandRequestBuilder;
 import com.zhaocai.common.signature.service.SignatureCommandFactory;
 import com.zhaocai.common.signature.service.command.PersonAuthCommand;
 import com.zhaocai.system.api.domain.BusinessUser;
+import com.zhaocai.system.api.domain.SysUser;
 import com.zhaocai.system.api.system.RemoteUserService;
 import lombok.extern.slf4j.Slf4j;
 import net.qiyuesuo.v3sdk.model.auth.response.UserauthAuthurl2Response;
@@ -274,17 +275,22 @@ public class VendorContactServiceImpl extends ServiceImpl<VendorContactMapper,Ve
 
     @Override
     public Long addLoginUser(String contactPhone, String contactName) {
-        BusinessUser businessUser = new BusinessUser();
-        businessUser.setUserName(contactPhone);
-        businessUser.setNickName(contactName);
-        businessUser.setUserType(UserTypeEnum.VENDOR);
-        R<Long> r = remoteUserService.addBusinessUser(businessUser, SecurityConstants.INNER);
+        SysUser user = remoteUserService.getUserInfoByUsername(contactPhone, SecurityConstants.INNER);
+        if (user != null) {
+            return user.getUserId();
+        } else {
+            BusinessUser businessUser = new BusinessUser();
+            businessUser.setUserName(contactPhone);
+            businessUser.setNickName(contactName);
+            businessUser.setUserType(UserTypeEnum.VENDOR);
+            R<Long> r = remoteUserService.addBusinessUser(businessUser, SecurityConstants.INNER);
 
-        if (R.SUCCESS != r.getCode()) {
-            throw new BusinessException(r.getMsg());
+            if (R.SUCCESS != r.getCode()) {
+                throw new BusinessException(r.getMsg());
+            }
+
+            return r.getData();
         }
-
-        return r.getData();
     }
 
     @Override
