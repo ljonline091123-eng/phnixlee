a87ae15c2d23dcb960ac21f584993fa8b1f9c8d5
xwj <xwj@qq.com>
2024-12-10 09:43:44 +0800

    文档中台-添加书签，校验上传文件名大小，上传minio修改文件名

---
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/agreement/vo/res/AgreementBookmarkVO.java
@@ -0,0 +1,89 @@
+package com.zhaocai.business.agreement.vo.res;
+
+import com.zhaocai.business.common.annotations.DictCache;
+import com.zhaocai.business.common.base.AdviceObject;
+import com.zhaocai.business.common.enums.DictBizEnum;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class AgreementBookmarkVO extends AdviceObject {
+    @ApiModelProperty(value = "合同名称")
+    private String agreementName;
+
+    @ApiModelProperty(value = "合同编号")
+    private String agreementCode;
+
+    @ApiModelProperty(value = "合同签订金额(含税)")
+    private BigDecimal totalAmountIncTax;
+
+    @ApiModelProperty(value = "单位内部合同管理编码")
+    private String innerAgreementCode;
+
+    @ApiModelProperty(value = "甲方名称")
+    private String partyAName;
+
+    @ApiModelProperty(value = "乙方名称")
+    private String partyBName;
+
+    @ApiModelProperty(value = "支出业务分类")
+    private Integer expenditureBusinessType;
+
+    @ApiModelProperty(value = "交易标的物名称")
+    private String subjectMatterName;
+
+    @ApiModelProperty(value = "支付周期")
+    private String paymentCycle;
+
+    @ApiModelProperty(value = "支付方式")
+    private String paymentWay;
+
+
+    @ApiModelProperty(value = "乙方法人代表")
+    private String partyBLegalName;
+
+    @ApiModelProperty(value = "乙方法人代表身份证")
+    private String partyBLegalIdCard;
+
+    @ApiModelProperty(value = "乙方法人代表联系方式")
+    private String partyBLegalPhone;
+
+    @ApiModelProperty(value = "乙方现场实际履职负责人")
+    private String partyBResponsibleName;
+
+    @ApiModelProperty(value = "乙方现场实际履职负责人身份证")
+    private String partyBResponsibleIdCard;
+
+    @ApiModelProperty(value = "乙方现场实际履职负责人联系方式")
+    private String partyBResponsiblePhone;
+
+    @ApiModelProperty(value = "合同履行地")
+    private String agreementPerformAddress;
+
+    @ApiModelProperty(value = "国家地区代码(履行地)")
+    private String agreementPerformCountry;
+
+    @ApiModelProperty(value = "行政区划代码(履行地)")
+    private String agreementPerformDistrict;
+
+    @ApiModelProperty(value = "计租方式")
+    private Integer rentalMethod;
+
+    @ApiModelProperty(value = "支出业务分类")
+    @DictCache(dictBizEnum = DictBizEnum.PROCUREMENT_PLAN_TYPE,filedName = "expenditureBusinessType")
+    private String expenditureBusinessTypeText;
+
+    @ApiModelProperty(value = "支付周期")
+    private String paymentCycleText;
+
+    @ApiModelProperty(value = "支付方式")
+    private String paymentWayText;
+
+    @DictCache(dictBizEnum = DictBizEnum.AGREEMENT_RENTAL_METHOD,filedName = "rentalMethod")
+    @ApiModelProperty(value = "计租方式")
+    private String rentalMethodText;
+
+
+}
--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/pub/service/impl/AttachmentServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/pub/service/impl/AttachmentServiceImpl.java
@@ -6,6 +6,7 @@ import cn.hutool.core.io.file.FileNameUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zhaocai.business.common.config.FileYOZOConfig;
 import com.zhaocai.business.common.enums.AttachmentTypeEnum;
 import com.zhaocai.business.common.exception.ParamValidateException;
 import com.zhaocai.business.common.utils.ValidateUtils;
@@ -13,12 +14,19 @@ import com.zhaocai.business.pub.domain.Attachment;
 import com.zhaocai.business.pub.mapper.AttachmentMapper;
 import com.zhaocai.business.pub.service.IAttachmentService;
 import com.zhaocai.business.pub.service.ISysFileService;
+import com.zhaocai.business.pub.utils.BookmarkUtils;
+import com.zhaocai.business.pub.utils.Sender;
+import com.zhaocai.business.pub.utils.YOZOfileUtils;
 import com.zhaocai.business.pub.vo.req.AttachmentRequestVO;
 import com.zhaocai.business.pub.vo.res.AttachmentVO;
+import com.zhaocai.business.sdk.bean.EditParams;
+import com.zhaocai.business.sdk.bean.PreviewParams;
 import com.zhaocai.business.vendor.vo.res.DownloadAgreementVO;
 import com.zhaocai.common.core.utils.NumberUtil;
 import com.zhaocai.common.core.utils.StringUtils;
 import com.zhaocai.common.core.utils.bean.BeanCopierUtil;
+import com.zhaocai.common.security.utils.SecurityUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +34,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.nio.file.Path;
 
 /**
  * 附件Service业务层处理
@@ -38,6 +47,180 @@ public class AttachmentServiceImpl extends ServiceImpl<AttachmentMapper, Attachm
 
     @Autowired
     private ISysFileService sysFileService;
+
+    @Autowired
+    private YOZOfileUtils yozOfileUtils;
+
+    @Autowired
+    private FileYOZOConfig fileYOZOConfig;
+
+    @Autowired
+    private BookmarkUtils bookmarkUtils;
+
+    @Autowired
+    private Sender sender;
+
+    //文档中台——获取预览word文件URL
+    @Override
+    public String  viewWordFileURL(String fileName, String fileUrl){
+        String HtmlName = yozOfileUtils.removeSuffix(fileName);
+        Path path = yozOfileUtils.downloadFile(fileUrl, yozOfileUtils.createTempFilePath(fileName));
+        try {
+            // 组织请求参数
+            PreviewParams params = new PreviewParams();
+            // 设置要预览的文件
+            params.setFilePath(path.toString());
+            params.setFileName(fileName);
+            params.setHtmlName(HtmlName);
+            params.setHtmlTitle(HtmlName);
+            // 是否可打印
+            params.setPrintMenu(true, false);
+            // 设置可下载
+            params.setDownloadMenu(true, fileName);
+            // 是否显示修订
+            params.setAcceptTracks(false);
+            // 允许复制
+            params.setCopy(false);
+            // 只允许打开一次
+            params.setPreviewNumber(5);
+            String response= sender.post(PreviewParams.URL_PREVIEW, PreviewParams.CONVERT_TYPE_PREVIEW_OFFICE, params.getRequestBody());
+            System.out.println("预览Office文件响应结果：");
+            System.out.println(response);
+            String viewUrl = new JSONObject(response).optJSONObject("data").optString("viewUrl");
+            String newViewUrl = yozOfileUtils.updateFileUrl(viewUrl);
+            System.out.println(newViewUrl);
+            //删除生成的临时文件
+            System.out.println("删除文件路径:" + path.toString());
+            yozOfileUtils.deleteTempFilePath(path.toString());
+            return newViewUrl;
+        } catch (Exception e) {
+            throw new RuntimeException("生成文件预览url失败", e);
+        }
+
+    }
+
+    //文档中台——获取预览PDF文件URL
+    @Override
+    public String  viewPDFFileURL(String fileName, String fileUrl){
+        String HtmlName = yozOfileUtils.removeSuffix(fileName);
+        Path path = yozOfileUtils.downloadFile(fileUrl, yozOfileUtils.createTempFilePath(fileName));
+        try {
+            // 组织请求参数
+            PreviewParams params = new PreviewParams();
+            // 设置要预览的文件
+            params.setFilePath(path.toString());
+            params.setFileName(fileName);
+            params.setHtmlName(HtmlName);
+            params.setHtmlTitle(HtmlName);
+            // 是否可打印
+            params.setPrintMenu(true, false);
+            // 设置可下载
+            params.setDownloadMenu(true, fileName);
+            // 允许复制
+            params.setCopy(true);
+            String response = sender.post(PreviewParams.URL_PREVIEW, PreviewParams.CONVERT_TYPE_PREVIEW_PDF, params.getRequestBody());
+            System.out.println("预览Office文件响应结果：");
+            System.out.println(response);
+            String viewUrl = new JSONObject(response).optJSONObject("data").optString("viewUrl");
+            String newViewUrl = yozOfileUtils.updateFileUrl(viewUrl);
+            System.out.println(newViewUrl);
+            //删除生成的临时文件
+            System.out.println("删除文件路径:" + path.toString());
+            yozOfileUtils.deleteTempFilePath(path.toString());
+            return newViewUrl;
+        } catch (Exception e) {
+            throw new RuntimeException("生成文件预览url失败", e);
+        }
+
+    }
+
+    //文档中台——获取预览图片URL
+    @Override
+    public String  viewImageURL(String fileName, String fileUrl){
+        String HtmlName = yozOfileUtils.removeSuffix(fileName);
+        Path path = yozOfileUtils.downloadFile(fileUrl, yozOfileUtils.createTempFilePath(fileName));
+        try {
+            // 组织请求参数
+            PreviewParams params = new PreviewParams();
+            // 设置要预览的文件
+            params.setFilePath(path.toString());
+            params.setFileName(fileName);
+            params.setHtmlName(HtmlName);
+            params.setHtmlTitle(HtmlName);
+            // 是否可打印
+            params.setPrintMenu(true, false);
+            // 设置可下载
+            params.setDownloadMenu(true, fileName);
+            String response = sender.post(PreviewParams.URL_PREVIEW, PreviewParams.CONVERT_TYPE_PREVIEW_PIC, params.getRequestBody());
+            System.out.println("预览图片文件响应结果：");
+            System.out.println(response);
+            String viewUrl = new JSONObject(response).optJSONObject("data").optString("viewUrl");
+            String newViewUrl = yozOfileUtils.updateFileUrl(viewUrl);
+            System.out.println(newViewUrl);
+            //删除生成的临时文件
+            System.out.println("删除文件路径:" + path.toString());
+            yozOfileUtils.deleteTempFilePath(path.toString());
+            return newViewUrl;
+        } catch (Exception e) {
+            throw new RuntimeException("生成文件预览url失败", e);
+        }
+
+    }
+
+    //文档中台——获取编辑word文档的URL
+    @Override
+    public String  editWordURL(Long attachmentId, String fileName, String fileUrl){
+        Path path = yozOfileUtils.downloadFile(fileUrl, yozOfileUtils.createTempFilePath(fileName));
+        //当前登录用户信息
+        String loginUserName = SecurityUtils.getUsername();
+        String nickName = SecurityUtils.getLoginUserNickName();
+        try {
+            // 编辑文档的-组织请求参数
+            EditParams params = new EditParams();
+            params.setFilePath(path.toString());
+            params.setFileName(fileName);
+            params.setUserInfo(loginUserName,nickName);
+            params.setUserRight(EditParams.USERRIGHT_EDIT);
+            params.setFileUUID(attachmentId.toString());
+            // 自动保存
+            params.setSaveFlag(true);
+            // 回调地址支持2中方式获取文件，请根据需要按照接口规范实现接口
+            params.setCallbackUrl(fileYOZOConfig.getCallbackUrl());
+            // 是否可打印
+            params.setPrintMenu(true, false);
+            // 设置可下载
+            params.setDownloadMenu(true, "");
+            // 设置文档显示比例，不设置则按照文档中保存的比例显示
+            params.setPageZoom(100);
+            // 开档打开修订
+            params.trackRevisionsOpen();
+            // 开档关闭修订
+//            params.trackRevisionsClose();
+            // 显示修订记录
+            params.trackRevisionsShow();
+            // 隐藏修订记录
+            // params.trackRevisionsHidden();
+            params.trackRevisionsAcceptRejectEnable();
+            // 清稿（修订记录全部接受）
+            // params.trackRevisionsClear();
+            // 设置复制粘贴剪切是否可用
+            params.setCopyPasteState(true, true, false);
+            //  设置书签时可选择的内容
+            params.setBookMarkListRange(bookmarkUtils.getBookmarkLabel(),true,false);
+
+            String response = sender.post(EditParams.URL_EDIT, EditParams.CONVERT_TYPE_EDIT, params.getRequestBody());
+            System.out.println("编辑响应结果：");
+            System.out.println(response);
+            String editUrl = new JSONObject(response).optJSONObject("data").optString("editUrl");
+            String newEditUrl = yozOfileUtils.updateFileUrl(editUrl);
+            System.out.println(newEditUrl);
+            return newEditUrl;
+        } catch (Exception e) {
+            throw new RuntimeException("生成文件编辑url失败", e);
+        }
+
+    }
+
 
     @Override
     public void addAttachment(List<AttachmentRequestVO> attachmentList, AttachmentTypeEnum businessType, Long businessId) {
