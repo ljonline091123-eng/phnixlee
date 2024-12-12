aaf028623a6b830c6d3d9759f05bed6ccf9361ff
19974999539 <1540125496@qq.com>
2024-12-12 18:00:49 +0800

    提交日志信息

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/bidding/service/impl/BiddingMarkTemplateServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/bidding/service/impl/BiddingMarkTemplateServiceImpl.java
@@ -20,21 +20,16 @@ import com.zhaocai.business.bidding.vo.res.BiddingMarkCategoryDetailVO;
 import com.zhaocai.business.bidding.vo.res.BiddingMarkItemDetailVO;
 import com.zhaocai.business.bidding.vo.res.BiddingMarkTemplateDetailVO;
 import com.zhaocai.business.bidding.vo.res.BiddingMarkTemplateListVO;
-import com.zhaocai.business.common.enums.DeptEnum;
 import com.zhaocai.business.common.exception.BusinessException;
 import com.zhaocai.business.common.utils.ValidateUtils;
-import com.zhaocai.business.pub.vo.res.TemplateListVO;
 import com.zhaocai.common.core.bean.PageResult;
 import com.zhaocai.common.core.constant.NumberConstant;
 import com.zhaocai.common.core.constant.SecurityConstants;
-import com.zhaocai.common.core.constant.UserConstants;
 import com.zhaocai.common.core.text.Convert;
 import com.zhaocai.common.core.utils.StringUtils;
 import com.zhaocai.common.core.utils.bean.BeanCopierUtil;
-import com.zhaocai.common.core.web.domain.AjaxResult;
 import com.zhaocai.common.security.utils.SecurityUtils;
 import com.zhaocai.system.api.domain.SysDept;
-import com.zhaocai.system.api.domain.SysUser;
 import com.zhaocai.system.api.system.RemoteSystemService;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,7 +38,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -143,15 +137,17 @@ public class BiddingMarkTemplateServiceImpl extends ServiceImpl<BiddingMarkTempl
 ////                sysDeptList = remoteSystemService.getDeptByThridDeptId(currUserTowLevelThridDeptId,SecurityConstants.INNER);
 //            }
 //        }
-        if (CollectionUtil.isNotEmpty(sysDeptList)) {
-            List<String> deptIdList = sysDeptList.stream().map(dept -> dept.getDeptId()+"").collect(Collectors.toList());
-            String id=String.join(",",deptIdList);
-            String[] deptIds = Convert.toStrArray(id);
-            queryVO.setUseUnit(deptIds);
-            IPage<BiddingMarkTemplateListVO> pages = baseMapper.selectList(queryVO.toMybatisPage(),queryVO);
-            return pages;
-        }
-        return null;
+//        if (CollectionUtil.isNotEmpty(sysDeptList)) {
+//            List<String> deptIdList = sysDeptList.stream().map(dept -> dept.getDeptId()+"").collect(Collectors.toList());
+//            String id=String.join(",",deptIdList);
+//            String[] deptIds = Convert.toStrArray(id);
+//            queryVO.setUseUnit(deptIds);
+//            IPage<BiddingMarkTemplateListVO> pages = baseMapper.selectList(queryVO.toMybatisPage(),queryVO);
+//            return pages;
+//        }
+        IPage<BiddingMarkTemplateListVO> pages = baseMapper.selectList(queryVO.toMybatisPage(),queryVO);
+        return pages;
+     //   return null;
     }
 
     /**
