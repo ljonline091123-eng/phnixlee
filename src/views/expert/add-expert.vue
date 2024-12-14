38467ecf75d273f587f610a7a10734e0dba92182
zs <1371576853@qq.com>
2024-12-14 15:51:16 +0800

    专家保存修改bug修复，文件预览

--- src/views/expert/add-expert.vue
+++ src/views/expert/add-expert.vue
@@ -205,7 +205,7 @@
                   style="width: 100%"
                   placeholder="选择日期"
                   value-format="yyyy-MM-dd"
-                  :disabled="formData.id ? true : false || isSubmit"
+                  :disabled="isSubmit"
                 />
               </el-form-item>
             </el-col>
@@ -237,7 +237,7 @@
                   style="width: 100%"
                   placeholder="选择日期"
                   value-format="yyyy-MM-dd"
-                  :disabled="formData.id ? true : false || isSubmit"
+                  :disabled="isSubmit"
                 />
               </el-form-item>
             </el-col>
@@ -276,7 +276,7 @@
                   style="width: 100%"
                   placeholder="选择日期"
                   value-format="yyyy-MM-dd"
-                  :disabled="formData.id ? true : false || isSubmit"
+                  :disabled="isSubmit"
                 />
               </el-form-item>
             </el-col>
@@ -438,6 +438,19 @@
       :loading="calibrateLoading"
       @update:visible="calibrateVisible = $event"
   />
+
+    <el-dialog
+      :title="templateDialogTitle"
+      :visible.sync="templateDialogVisible"
+      width="80%"
+    >
+      <iframe allowfullscreen="true"
+              :src= this.viewFileUrl
+              width="100%"
+              height="700px"
+              frameborder="0"
+      ></iframe>
+    </el-dialog>
   </div>
 </template>
 
@@ -456,6 +469,7 @@ import {
   getProcessLogList, getProcessLogListNew,
 } from "@/api/procurement/manage";
 import {showSecretRelatedTips} from "@/utils/MyUtils";
+import {getViewAttachmentURLByID} from "@/api/template/file";
 export default {
   name: "add-expert",
   dicts: [
@@ -475,6 +489,10 @@ export default {
     return {
       id:'',
       type:'',
+      /* 合同模板联想文档预览 */
+      templateDialogTitle: "",
+      templateDialogVisible: false,
+      viewFileUrl:"",
       businessTypeList:[],
       expertTypeList:[],
       expertVisible:false,
@@ -710,13 +728,40 @@ export default {
       } catch (error) {}
     },
     //点击文件列表中已上传文件进行下载
-    handlePreview(file) {
-      var a = document.createElement('a');
-      var event = new MouseEvent('click');
-      a.download = file.name;
-      a.href = file.fileUrl;
-      a.dispatchEvent(event);
+    async handlePreview(file) {
       console.log(file)
+      // var a = document.createElement('a');
+      // var event = new MouseEvent('click');
+      // a.download = file.name;
+      // a.href = file.fileUrl;
+      // a.dispatchEvent(event);
+      // console.log(file)
+
+
+      // if (file.url) {
+      //   // 在新标签页打开文件
+      //   window.open(file.url, "_blank");
+      // } else {
+      //   this.$message.error("文件无法预览，缺少 URL");
+      // }
+
+
+      //获取附件的预览URL
+      if (file.id) {
+        this.templateDialogTitle = file.name + "预览";
+        this.templateDialogVisible = true;
+        //获取文档中台的文档编辑URL
+        try {
+          const res = await getViewAttachmentURLByID({attachmentId: file.id});
+          this.viewFileUrl = res.data;
+          console.log("viewFileUrl:", this.viewFileUrl);
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        console.error('专家附件 数据未正确加载');
+      }
+
     },
 
     async getInfoDetail(id) {
@@ -736,7 +781,18 @@ export default {
         if(data.technicalTitles){
           this.formData.technicalTitles=data.technicalTitles+""
         }
-        // SAVE(0,"保存"),
+
+      // 遍历数组，新增 name 属性,给文件组件显示文件名称
+      this.formData.resumeAttachList = this.formData.resumeAttachList.map(item => {
+        return {
+          ...item, // 保留原有属性
+          name: item.fileName, // 将 fileName 的值赋给 name
+          url: item.fileUrl // 将 fileUrl 的值赋给 url
+        };
+      });
+
+
+      // SAVE(0,"保存"),
         // IN_APPROVAL(1,"审批中"),
         // REJECT(2,"审批拒绝"),
         // APPROVE(3,"审批通过")
@@ -814,14 +870,12 @@ export default {
       console.log("sub");
     },
     async fileSuccess(res) {
+      this.formData.resumeAttachList = null;
       const { url, name } = res.data;
-      this.formData.resumeAttachList = [{ fileName: name, fileUrl: url }];
-      this.$refs.fileFormRef.clearValidate("fileTemplate");
+      this.formData.resumeAttachList = [{ fileName: name, fileUrl: url,name: name, url: url }];
     },
     fileRemove() {
-      this.$set(this.formData, "fileList", []);
-      this.$set(this.formData, "fileTemplate", []);
-      this.attachmentId = "";
+      this.$set(this.formData, "resumeAttachList", []);
     },
   },
   watch: {
