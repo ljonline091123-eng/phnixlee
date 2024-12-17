40b2b5984c7728f166a46887cf700a6586f16182
19974999539 <1540125496@qq.com>
2024-12-17 18:31:28 +0800

    删除标包

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/procurement/service/impl/MaterialsListServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/procurement/service/impl/MaterialsListServiceImpl.java
@@ -162,8 +162,8 @@ public class MaterialsListServiceImpl extends ServiceImpl<MaterialsListMapper, M
         }
 
         Map<String, List<MaterialsList>> map = materialsList.stream()
-                .collect(Collectors.groupingBy( x -> x.getSplitId() + "-_#_-" + (x.getSplitContractName()==null?"&":x.getSplitContractName())
-                                + "-_#_-" +(x.getContractScope()==null?"&":x.getContractScope()) ,
+                .collect(Collectors.groupingBy( x -> x.getSplitId() + "-_#_-" + (x.getSplitContractName().equals("")?"&":x.getSplitContractName())
+                                + "-_#_-" +(x.getContractScope().equals("")?"&":x.getContractScope()) ,
                         Collectors.mapping(materials -> BeanCopierUtil.copyBean(materials,MaterialsList.class),
                                 Collectors.toList())));
 
@@ -436,8 +436,8 @@ public class MaterialsListServiceImpl extends ServiceImpl<MaterialsListMapper, M
                 .filter(x -> NumberUtil.compare(x.getCount(),BigDecimal.ZERO) > 0 && x.getPushFlag().equals("N"))
                 .collect(Collectors.toList());
         Map<String, List<MaterialsList>> map = materialsList.stream()
-                .collect(Collectors.groupingBy( x -> x.getSplitId() + "-_#_-" + (x.getSplitContractName()==null?"&":x.getSplitContractName())
-                                + "-_#_-" +(x.getContractScope()==null?"&":x.getContractScope()) ,
+                .collect(Collectors.groupingBy( x -> x.getSplitId() + "-_#_-" + (x.getSplitContractName().equals("")?"&":x.getSplitContractName())
+                                + "-_#_-" +(x.getContractScope().equals("")?"&":x.getContractScope()) ,
                         Collectors.mapping(materials -> BeanCopierUtil.copyBean(materials,MaterialsList.class),
                                 Collectors.toList())));
 
