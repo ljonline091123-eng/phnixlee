af63737d1915f0d5a23eb7a40376441b55e4a9b7
xwj <xwj@qq.com>
2024-12-27 12:41:44 +0800

    修改合同时-无法提交合同，合同一直加载中

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/agreement/service/impl/AgreementServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/agreement/service/impl/AgreementServiceImpl.java
@@ -786,20 +786,36 @@ public class AgreementServiceImpl extends ServiceImpl<AgreementMapper,Agreement>
             throw new BusinessException("该状态下的合同不允许提交");
         }
 
-        if (NumberUtil.isNullOrZero(agreement.getLabelAttachmentId())) {
-            throw new BusinessException("合同附件正在生成中，请稍后重试");
+        //获取agreement的AttachmentId，复制文件名和文件url到LabelAttachmentId
+        if (agreement.getAttachmentId() != null) {
+            Attachment attachment = attachmentService.getById(agreement.getAttachmentId());
+            Long labelAttachmentId= attachmentService.addAttachment(new AttachmentRequestVO(attachment.getFileName(),attachment.getFileUrl()),AttachmentTypeEnum.AGREEMENT_ORIGINAL,id);
+            agreement.setLabelAttachmentId(labelAttachmentId);
+            // 只更新 LabelAttachmentId
+            super.update(new LambdaUpdateWrapper<Agreement>()
+                    .set(Agreement::getLabelAttachmentId,labelAttachmentId)
+                    .eq(Agreement::getId,id));
+        } else {
+            throw new BusinessException("合同附件不存在，请稍后重试");
         }
+
 
         // 状态调整为审批中
         super.update(new LambdaUpdateWrapper<Agreement>()
                 .set(Agreement::getAgreementState,AgreementStateEnum.IN_APPROVAL.getState())
                 .eq(Agreement::getId,id));
 
-        if (AgreementStateEnum.DRAFT.equalsState(agreement.getAgreementState())) {
-            // 初次提交，设置水印
-            Attachment attachment = attachmentService.getById(agreement.getLabelAttachmentId());
-            agreementFileZService.applyWatermark(agreement.getAgreementName(),agreement.getPartyAName(),attachment.getFileUrl(),agreement.getId());
-        }
+
+//        if (NumberUtil.isNullOrZero(agreement.getLabelAttachmentId())) {
+//            throw new BusinessException("合同附件正在生成中，请稍后重试");
+//        }
+        //联想文档设置水印
+//        if (AgreementStateEnum.DRAFT.equalsState(agreement.getAgreementState())) {
+//            // 初次提交，设置水印
+//            Attachment attachment = attachmentService.getById(agreement.getLabelAttachmentId());
+//            agreementFileZService.applyWatermark(agreement.getAgreementName(),agreement.getPartyAName(),attachment.getFileUrl(),agreement.getId());
+//        }
+
 
         //接入底层逻辑平台流程
         Map<String,Object> paramMap = new HashMap<>();
