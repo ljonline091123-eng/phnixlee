ecd67a29397230fb1cb08739d1e61bbd928d3680
zs <1371576853@qq.com>
2025-01-14 17:30:20 +0800

    增加流程记录类tb_bpm_log

---
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/process/mapper/BpmLogMapper.java
@@ -0,0 +1,10 @@
+package com.zhaocai.business.process.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhaocai.business.process.domain.BpmLog;
+
+
+public interface BpmLogMapper extends BaseMapper<BpmLog> {
+
+
+}
