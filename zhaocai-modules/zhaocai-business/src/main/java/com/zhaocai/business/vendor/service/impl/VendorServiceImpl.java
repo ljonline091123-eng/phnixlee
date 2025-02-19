a5819c4ee0366819cd25229f39193580d98e5907
19974999539 <1540125496@qq.com>
2025-02-19 16:48:39 +0800

    提交同步

--- zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorServiceImpl.java
+++ zhaocai-modules/zhaocai-business/src/main/java/com/zhaocai/business/vendor/service/impl/VendorServiceImpl.java
@@ -1044,7 +1044,7 @@ public class VendorServiceImpl extends ServiceImpl<VendorMapper,Vendor> implemen
                 //map.put("cust_mercht_modif_pre_name",  );//客商变更前名称(曾用名)
                 map.put("corp_princ_legal_rep",  bean.getLegalRepresentative());
                 map.put("unified_soci_crdt_cd",  bean.getSocialCreditCode());
-                map.put("rgst_cap", bean.getRegisteredCapital()==null? new BigDecimal(0):bean.getRegisteredCapital().multiply(new BigDecimal(10000)) );\
+                map.put("rgst_cap", bean.getRegisteredCapital()==null? new BigDecimal(0):bean.getRegisteredCapital().multiply(new BigDecimal(10000)) );
                 String range = bean.getBusinessScope();
                 if(range !=null &&range.length()>0){
                     range = range.replaceAll("\\n|\\r\\n", "");
