d7881095ceaff0b5f4b01d489735a499cc1c8e55
xb <276489267@qq.com>
2025-02-27 17:40:32 +0800

    密码错误提示

--- zhaocai-auth/src/main/java/com/zhaocai/auth/service/SysPasswordService.java
+++ zhaocai-auth/src/main/java/com/zhaocai/auth/service/SysPasswordService.java
@@ -62,7 +62,7 @@ public class SysPasswordService
             retryCount = retryCount + 1;
             recordLogService.recordLogininfor(username, Constants.LOGIN_FAIL, String.format("密码输入错误%s次", retryCount));
             redisService.setCacheObject(getCacheKey(username), retryCount, lockTime, TimeUnit.MINUTES);
-            throw new ServiceException("用户不存在/密码错误");
+            throw new ServiceException("密码错误");
         }
         else
         {
