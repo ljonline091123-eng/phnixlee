817e853916238c10d89145b5b222e06a11a4b642
liaozhiqin <735770595@qq.com>
2025-03-14 09:59:49 +0800

    退出登录时添加前缀

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -240,7 +240,7 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$store.dispatch('LogOut').then(() => {
-          location.href = '/index';
+          location.href = '/zhaocai';
         })
       }).catch(() => {
       });
